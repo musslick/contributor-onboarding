59991f50d03d8935278a0609c270a3dcac16ec03
denizmgun <denizmgun@gmail.com>
2024-07-29 14:06:52 +0200

    Added useless trash.

---
+++ src/trash.py
@@ -0,0 +1,8 @@
+'''
+This is a useless file, cluttering our repo
+but I think it's nice to just have it here in case we need to dump garbage code.
+
+'''
+
+if __name__ == "__main__":
+    print("Trash loaded.")
