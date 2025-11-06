951e9fabb09823ee582ae62e002afe3b8a332aba
Sebastian Musslick <sebastian@musslick.de>
2025-11-06 12:08:51 +0000

    added another todo to the same todo because it was necessary

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # TODO: make all functions work with strings as well
 # TODO: add a new cool calculator function
-# TODO: something todo
+# TODO: something todo and another todo
 
 def sum(a: int, b: int) -> int:
     '''
